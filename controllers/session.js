0ea7e9c320ca90a6c346a313f21748b490b20f40
Aaron Lipkin <aj17@me.com>
2017-08-17 15:28:04 -0500

    dropping sessions

--- controllers/session.js
+++ controllers/session.js
@@ -3,7 +3,6 @@ const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const router = express.Router();
 const User = require('../models/user.js');
-const getUserData = require('../bin/user.js');
 
 
 //start session
