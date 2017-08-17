cf934caa874e232ede5f135dec600853bd9ba31b
alexandraalday <alexandra.alday@gmail.com>
2017-08-17 07:29:12 -0700

    created serveerver.js

---
+++ server.js
@@ -0,0 +1,36 @@
+// dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const app = express();
+
+//middleware
+app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static('public'));
+
+
+
+
+
+
+app.get('/', (req, res)=>{
+	res.send('yeah boiiiiii');
+});
+
+
+
+
+
+//	MONGOOSE CONNECTION
+mongoose.connect('mongodb://localhost:27017/8bitweather');
+mongoose.connection.once('open', ()=>{
+  console.log('mongo is connected');
+});
+
+
+// port
+app.listen(3000, ()=>{
+	console.log('listening bro');
+});
