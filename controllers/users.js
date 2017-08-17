0a752bc92c507f079edc9c5073d7e8136cf030fb
alexandraalday <alexandra.alday@gmail.com>
2017-08-17 10:12:13 -0700

    typo in seed data fixed

--- controllers/users.js
+++ controllers/users.js
@@ -23,22 +23,22 @@ router.post('/', function(req, res){
 	User.create([
 	{
 		name: 'blinky',
-		image: 'https://orig01.deviantart.net/8557/f/2013/021/f/9/1blinkyghost_by_camdencc-d5s8ix5.gif'
+		image: 'https://orig01.deviantart.net/8557/f/2013/021/f/9/1blinkyghost_by_camdencc-d5s8ix5.gif',
 		description: 'wakka wakka wakka'
 	},
 	{
 		name: 'pinky',
-		image: 'https://orig12.deviantart.net/ecf8/f/2013/021/f/9/1pinkyghost_by_camdencc-d5saavl.gif'
+		image: 'https://orig12.deviantart.net/ecf8/f/2013/021/f/9/1pinkyghost_by_camdencc-d5saavl.gif',
 		description: 'wakka wakka wakka'
 	},
 	{
 		name: 'inky',
-		image: 'https://orig05.deviantart.net/f745/f/2013/021/d/3/1inkyghost_by_camdencc-d5sab38.gif'
+		image: 'https://orig05.deviantart.net/f745/f/2013/021/d/3/1inkyghost_by_camdencc-d5sab38.gif',
 		description: 'wakka wakka wakka'
 	},
 	{
 		name: 'clyde',
-		image: 'https://orig08.deviantart.net/977f/f/2013/021/0/2/1clydeghost_by_camdencc-d5sab7r.gif'
+		image: 'https://orig08.deviantart.net/977f/f/2013/021/0/2/1clydeghost_by_camdencc-d5sab7r.gif',
 		description: 'wakka wakka wakka'
 	}], (err, data) => {
     		res.redirect('/users')
